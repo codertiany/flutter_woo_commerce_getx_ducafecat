2dc37f6206d9a2583b644fbae941bedb78a1a365
ducafecat <ducafecat@gmail.com>
2022-03-09 23:23:48 +0800

    add -> 间距

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -3,11 +3,32 @@ import 'package:flutter/material.dart';
 import 'index.dart';
 
 class AppTheme {
+  /// 亮色主题
   static ThemeData light = ThemeData(
     colorScheme: lightColorScheme,
   );
 
+  /// 暗色主题
   static ThemeData dark = ThemeData(
     colorScheme: darkColorScheme,
   );
+
+  /// 列表行间距 - 10 上下
+  static EdgeInsets edgeListRow = const EdgeInsets.symmetric(vertical: 10);
+
+  /// 输入框间距 - 10, 20 上下，左右
+  static EdgeInsets edgeInput =
+      const EdgeInsets.symmetric(vertical: 10, horizontal: 20);
+
+  /// 卡片内间距 - 12 上下左右
+  static EdgeInsets edgeCard = const EdgeInsets.all(12);
+
+  /// 页面内边距 - 16 左右
+  static EdgeInsets edgePage = const EdgeInsets.symmetric(horizontal: 16);
+
+  /// 标题内容间 - 10
+  static EdgeInsets edgeTitleContent = const EdgeInsets.only(top: 10);
+
+  /// 段落间距 - 24
+  static EdgeInsets edgeParagraph = const EdgeInsets.only(top: 10);
 }
--- lib/common/widgets/text.dart
+++ lib/common/widgets/text.dart
@@ -1,10 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
+/// 文字 Widget
 class TextWidget extends StatelessWidget {
-  const TextWidget({Key? key}) : super(key: key);
+  final String text;
+  final TextStyle? style;
+  final int? maxLines; // 行数
+  final bool? softWrap; // 自动换行
+  final TextOverflow? overflow; // 溢出
+
+  const TextWidget({
+    Key? key,
+    required this.text,
+    this.style,
+    this.maxLines = 1,
+    this.softWrap = false,
+    this.overflow = TextOverflow.clip,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(
+      text,
+      style: style,
+      overflow: overflow,
+      maxLines: maxLines,
+      softWrap: softWrap,
+    );
   }
+
+  /// 文字 - 导航
+  TextWidget.navigation({
+    Key? key,
+    required this.text,
+    this.maxLines = 1,
+    this.softWrap = false,
+    this.overflow = TextOverflow.clip,
+  })  : style = Get.textTheme.headlineSmall?.copyWith(
+          fontSize: Get.textTheme.headlineSmall?.fontSize?.sp,
+        ),
+        super(key: key);
+
+  /// 文字 - 标题1
+  TextWidget.title1({
+    Key? key,
+    required this.text,
+    this.maxLines = 1,
+    this.softWrap = false,
+    this.overflow = TextOverflow.clip,
+  })  : style = Get.textTheme.headline5?.copyWith(
+          fontSize: Get.textTheme.headline5?.fontSize?.sp,
+        ),
+        super(key: key);
+
+  /// 文字 - 标题2
+  TextWidget.title2({
+    Key? key,
+    required this.text,
+    this.maxLines = 1,
+    this.softWrap = false,
+    this.overflow = TextOverflow.clip,
+  })  : style = Get.textTheme.headline6?.copyWith(
+          fontSize: 18.sp,
+        ),
+        super(key: key);
+
+  /// 文字 - 标题3
+  TextWidget.title3({
+    Key? key,
+    required this.text,
+    this.maxLines = 1,
+    this.softWrap = false,
+    this.overflow = TextOverflow.clip,
+  })  : style = Get.textTheme.headline6?.copyWith(
+          fontSize: 15.sp,
+        ),
+        super(key: key);
+
+  /// 文字 - 正文1
+  TextWidget.body1({
+    Key? key,
+    required this.text,
+    this.maxLines = 1,
+    this.softWrap = false,
+    this.overflow = TextOverflow.clip,
+  })  : style = Get.textTheme.bodyText1?.copyWith(
+          fontSize: 15.sp,
+        ),
+        super(key: key);
+
+  /// 文字 - 正文2
+  TextWidget.body2({
+    Key? key,
+    required this.text,
+    this.maxLines = 1,
+    this.softWrap = false,
+    this.overflow = TextOverflow.clip,
+  })  : style = Get.textTheme.bodyText1?.copyWith(
+          fontSize: 12.sp,
+        ),
+        super(key: key);
+
+  /// 文字 - 正文3
+  TextWidget.body3({
+    Key? key,
+    required this.text,
+    this.maxLines = 1,
+    this.softWrap = false,
+    this.overflow = TextOverflow.clip,
+  })  : style = Get.textTheme.bodyText1?.copyWith(
+          fontSize: 9.sp,
+        ),
+        super(key: key);
 }
